3028bc2fe550333f109035da853da303d9edab2d
owensource <ciruz001@cougars.csusm.edu>
2018-08-22 08:34:29 -0700

    eslint-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
+/* eslint-disable */
+
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js', 
-        path: path.resolve(__dirname, './dist')
-    },
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.(xml)$/,
-                use: [
-                    'xml-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader',
+        ],
+      },
+    ],
+  },
 };
